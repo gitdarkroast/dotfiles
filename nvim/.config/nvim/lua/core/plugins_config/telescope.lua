dd1b55449635f523381b7e66ee6cb05acd6a9b5f
Hanif Ladha <hladha@ABT104335.local>
2024-04-13 09:14:14 -0700

    Update telescope config

--- nvim/.config/nvim/lua/core/plugins_config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugins_config/telescope.lua
@@ -1,17 +1,28 @@
-local telescope = require('telescope')
-local builtin = require('telescope.builtin')
-local actions = require('telescope.actions')
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+local actions = require("telescope.actions")
+local transform_mod = require("telescope.actions.mt").transform_mod
+
+local trouble = require("trouble")
+local trouble_telescope = require("trouble.providers.telescope")
+
+local custom_actions = transform_mod({
+	open_trouble_qflist = function(prompt_buffer)
+		trouble.toggle("quickfix")
+	end,
+})
 
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-      }
-    }
-  }
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous, -- move to prev result
+				["<C-j>"] = actions.move_selection_next, -- move to next result
+				["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+				["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+			},
+		},
+	},
 })
 
 -- load fuzzy finder
@@ -20,12 +31,12 @@ telescope.load_extension("fzf")
 local wk = require("which-key")
 
 wk.register({
-  f = {
-    name = "find",
-    f = {"<cmd>Telescope find_files<cr>", "Fuzzy find files in cwd"},
-    r = {"<cmd>Telescope oldfiles<cr>", "Fuzzy find recent files"},
-    g = {"<cmd>Telescope live_grep<cr>", "Find string in cwd"},
-    c = {"<cmd>Telescope grep_string<cr>", "Find string under cursor cwd"},
-    t = {"<cmd>Telescope help_tags<cr>", "Find Tags"},
-  },
-}, {prefix = "<leader>"})
+	f = {
+		name = "find",
+		f = { "<cmd>Telescope find_files<cr>", "Fuzzy find files in cwd" },
+		r = { "<cmd>Telescope oldfiles<cr>", "Fuzzy find recent files" },
+		d = { "<cmd>Telescope live_grep<cr>", "Find string in cwd" },
+		c = { "<cmd>Telescope grep_string<cr>", "Find string under cursor cwd" },
+		t = { "<cmd>TodoTelescope<cr>", "Find ToDos" },
+	},
+}, { prefix = "<leader>" })
